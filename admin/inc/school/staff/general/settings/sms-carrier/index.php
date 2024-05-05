02ed0edbe50c4e02536f248ad67503d08de81a7f
coderjahidul <sobuz0349@gmail.com>
2024-05-05 17:24:32 +0600

    add bulksmsbd prining list link

--- admin/inc/school/staff/general/settings/sms-carrier/index.php
+++ admin/inc/school/staff/general/settings/sms-carrier/index.php
@@ -1196,6 +1196,20 @@ $school_logixsms_sender_id = $settings_logixsms['sender_id'];
 						</div>
 					</div>
 				</div>
+				<div class="wlsm_sms_carrier wlsm_bulksmsbd">
+					<div class="row">
+						<div class="col-md-3">
+							<label class="wlsm-font-bold"><?php esc_html_e('SMS Package', 'school-management'); ?>:</label>
+						</div>
+						<div class="col-md-9">
+							<div class="form-group">
+								<a class="wlsm-font-bold" target="_blank" href="https://bulksmsbd.com/bulksms-price.php">
+									<?php esc_html_e('Click for SMS Package Features and Pricing', 'school-management'); ?>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 
 				<div class="wlsm_sms_carrier wlsm_kivalosolutions">
 					<div class="row">
